0a37328e8070f1e74aacc339e3946b081e7b1cf4
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-07-06 22:34:26 -0300

    whyred: Track and include MiuiCamera
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,23 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+# Camera
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+
+# Connectivity Engine support (CNE)
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.quicinc.cne \
+    services-ext
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.sdm660 \
